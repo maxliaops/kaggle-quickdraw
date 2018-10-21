7aae8c11a824bf7981066d78d2ed54fc264ced18
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-21 13:44:19 +0200

    calculate the accuracy mean as a float

--- metrics.py
+++ metrics.py
@@ -3,4 +3,4 @@ import torch
 
 def accuracy(predictions, categories):
     _, predicted_categories = torch.max(predictions, 1)
-    return (predicted_categories == categories).sum() / categories.size(0)
+    return (predicted_categories == categories).sum().float() / categories.size(0)
