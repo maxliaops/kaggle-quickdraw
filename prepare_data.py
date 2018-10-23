6d43d6bfbf4961724f04a5238d93ed9bd4e9fb09
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-23 23:40:53 +0200

    create hd5f file

---
+++ prepare_data.py
@@ -0,0 +1,27 @@
+import glob
+import os
+
+import h5py
+import numpy as np
+import pandas as pd
+
+from utils import draw_it
+
+with open("/storage/kaggle/quickdraw/categories.txt") as categories_file:
+    categories = [l.rstrip("\n") for l in categories_file.readlines()]
+
+with h5py.File("quickdraw_train.hdf5", "w", libver="latest") as data_file:
+    for csv_file in glob.glob("/storage/kaggle/quickdraw/train_simplified_shard_0/*.csv"):
+        print("processing file '{}'".format(csv_file), flush=True)
+
+        df = pd.read_csv(
+            csv_file,
+            index_col="key_id",
+            converters={"drawing": lambda drawing: draw_it(eval(drawing), size=32)})
+
+        category = os.path.basename(csv_file)[:-6]
+        thumbnail = np.stack(df.drawing.values)
+
+        group = data_file.create_group(category)
+        group["thumbnail"] = thumbnail
+        group["category"] = [categories.index(word) for word in df.word]
---
+++ prepare_data.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+set -e
+set -o pipefail
+
+function install_dependencies() {
+  apt-get update >/dev/null
+  apt-get -y install python3-dev libsm-dev libxrender1 libxext6 zip git >/dev/null
+  rm -rf /var/lib/apt/lists/*
+
+  pip -q install virtualenv
+  virtualenv env --python=python3
+  . env/bin/activate
+
+  pip -q install -r requirements.txt
+}
+
+install_dependencies
+
+python prepare_data.py
