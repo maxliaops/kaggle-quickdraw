34b3204fdae9cac30602235ee59433b585c8bfce
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-24 23:43:33 +0200

    expand the number of channels to 3 inside the resnet model

--- models/common.py
+++ models/common.py
@@ -4,3 +4,11 @@ import torch.nn as nn
 class Flatten(nn.Module):
     def forward(self, x):
         return x.view(x.size(0), -1)
+
+
+class ExpandChannels2d(nn.Module):
+    def __init__(self, num_channels):
+        self.num_channels = num_channels
+
+    def forward(self, x):
+        return x.expand(-1, self.num_channels, -1, -1)
--- models/resnet.py
+++ models/resnet.py
@@ -1,11 +1,13 @@
 import torch.nn as nn
 from torchvision.models import resnet34
+from common import ExpandChannels2d
 
 
 class ResNet34(nn.Module):
     def __init__(self):
         super().__init__()
+        self.expand_channels = ExpandChannels2d(3)
         self.resnet = resnet34(pretrained=True)
 
     def forward(self, x):
-        return self.resnet(x)
+        return self.resnet(self.expand_channels(x))
