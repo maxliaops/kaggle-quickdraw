f8e3ddba1da1c7cd56ff817be9b6ea25c3412e40
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-25 23:14:21 +0200

    adapt tensor shape to calculate the accuracy

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -4,8 +4,4 @@ from .average_precision import mapk
 
 def accuracy(prediction_logits, categories, topk=3):
     _, predicted_categories = torch.topk(prediction_logits, topk, dim=1)
-    print("foo")
-    print(categories.shape)
-    print(predicted_categories.shape)
-    print("")
-    return mapk(categories.cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
+    return mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
