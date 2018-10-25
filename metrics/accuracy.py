db14b7c279eb404deab61ab218559a2af847f176
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-25 23:03:22 +0200

    added debug logs

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -4,4 +4,8 @@ from .average_precision import mapk
 
 def accuracy(prediction_logits, categories, topk=3):
     _, predicted_categories = torch.topk(prediction_logits, topk, dim=1)
+    print("foo")
+    print(categories.shape)
+    print(predicted_categories.shape)
+    print("")
     return mapk(categories.cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
