1fc007ca9c1f1cc4cab5c796232da552396ce505
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-25 22:59:10 +0200

    turn data into numpy arrays

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -4,4 +4,4 @@ from .average_precision import mapk
 
 def accuracy(prediction_logits, categories, topk=3):
     _, predicted_categories = torch.topk(prediction_logits, topk, dim=1)
-    return mapk(categories, predicted_categories, k=topk)
+    return mapk(categories.cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
