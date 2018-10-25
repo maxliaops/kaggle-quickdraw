9deaf77f1177b02da0ba1ce0e59de8c886e08e17
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-25 23:28:04 +0200

    added own implementation of map@3

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -4,4 +4,10 @@ from .average_precision import mapk
 
 def accuracy(prediction_logits, categories, topk=3):
     _, predicted_categories = torch.topk(prediction_logits, topk, dim=1)
-    return mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
+    p = mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
+    print("{} vs. {}".format(p, accuracy2(prediction_logits, categories, topk)))
+    return p
+
+def accuracy2(prediction_logits, categories, topk=3):
+    _, predicted_categories = torch.topk(prediction_logits, topk, dim=1)
+    return (predicted_categories == categories.unsqueeze(1).expand(-1, topk)).sum().float() / categories.size(0)
