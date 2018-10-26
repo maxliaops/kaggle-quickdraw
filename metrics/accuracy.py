c5e4fec386a73cceb1886dc2b5dbe915c9d16466
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-27 00:45:25 +0200

    explicitly state that the topk results should be sorted

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -3,5 +3,5 @@ from .average_precision import mapk
 
 
 def accuracy(prediction_logits, categories, topk=3):
-    _, predicted_categories = torch.topk(prediction_logits, topk, dim=1)
+    _, predicted_categories = torch.topk(prediction_logits, topk, dim=1, sorted=True)
     return mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
