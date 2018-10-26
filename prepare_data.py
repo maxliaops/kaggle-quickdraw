d682f3afd936ca01678f5147f5343325e5c51409
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-26 15:17:14 +0200

    loop over the categories

--- prepare_data.py
+++ prepare_data.py
@@ -1,5 +1,3 @@
-import glob
-import os
 import shutil
 
 import h5py
@@ -25,7 +23,9 @@ def prepare_strokes():
     categories = read_categories()
 
     with h5py.File("quickdraw_train.hdf5", "w", libver="latest") as data_file:
-        for csv_file in glob.glob("/storage/kaggle/quickdraw/train_simplified_shard_0/*.csv"):
+        for category in categories:
+            csv_file = "/storage/kaggle/quickdraw/train_simplified_shard_0/{}-0.csv".format(category)
+
             print("processing file '{}'".format(csv_file), flush=True)
 
             df = pd.read_csv(
@@ -33,11 +33,8 @@ def prepare_strokes():
                 index_col="key_id",
                 converters={"drawing": lambda drawing: eval(drawing)})
 
-            category = os.path.basename(csv_file)[:-6]
-
             group = data_file.create_group(category)
 
-            # TODO: use enum type
             group["category"] = [categories.index(word) for word in df.word]
 
             stroke_dtype = h5py.special_dtype(vlen=np.dtype("uint8"))
@@ -57,7 +54,9 @@ def prepare_thumbnails():
     categories = read_categories()
 
     with h5py.File("quickdraw_train_thumbnails.hdf5", "w", libver="latest") as data_file:
-        for csv_file in glob.glob("/storage/kaggle/quickdraw/train_simplified_shard_0/*.csv"):
+        for category in categories:
+            csv_file = "/storage/kaggle/quickdraw/train_simplified_shard_0/{}-0.csv".format(category)
+
             print("processing file '{}'".format(csv_file), flush=True)
 
             df = pd.read_csv(
@@ -65,7 +64,6 @@ def prepare_thumbnails():
                 index_col="key_id",
                 converters={"drawing": lambda drawing: draw_it(eval(drawing), size=32)})
 
-            category = os.path.basename(csv_file)[:-6]
             thumbnail = np.stack(df.drawing.values)
 
             group = data_file.create_group(category)
