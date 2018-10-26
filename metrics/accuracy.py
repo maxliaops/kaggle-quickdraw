74fbecc297319a9bd0540287dae4e12a1e503b38
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-27 00:53:16 +0200

    Revert "calculate the softmax before evaluating the accuracy"
    This reverts commit 7e3175ee93eee08f2b71a761f0c78fa73be3ac26.

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -1,9 +1,7 @@
 import torch
-import torch.nn.functional as F
 from .average_precision import mapk
 
 
 def accuracy(prediction_logits, categories, topk=3):
-    predictions = F.softmax(prediction_logits, dim=1)
-    _, predicted_categories = torch.topk(predictions, topk, dim=1, sorted=True)
+    _, predicted_categories = torch.topk(prediction_logits, topk, dim=1, sorted=True)
     return mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
