ff600916e0d96731a97472ede22425e174f991b4
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-28 15:49:21 +0100

    extract common code for convolutional blocks

--- models/cnn.py
+++ models/cnn.py
@@ -1,5 +1,22 @@
 import torch.nn as nn
+
 from .common import Flatten
+
+
+class ConvBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, padding):
+        super().__init__()
+        self.delegate = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, padding=padding),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=kernel_size, padding=padding),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self, x):
+        return self.delegate(x)
 
 
 class SimpleCnn(nn.Module):
@@ -10,19 +27,9 @@ class SimpleCnn(nn.Module):
 
         self.delegate = nn.Sequential(
             nn.BatchNorm2d(1),
-            nn.Conv2d(1, 32, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(32),
-            nn.Conv2d(32, 32, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(32),
+            ConvBlock(1, 32, kernel_size=5, padding=2),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(32, 64, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(64),
-            nn.Conv2d(64, 64, kernel_size=5, padding=2),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(64),
+            ConvBlock(32, 64, kernel_size=5, padding=2),
             nn.MaxPool2d(kernel_size=2, stride=2),
             Flatten(),
             nn.Linear(64 * last_layer_size ** 2, 1024),
