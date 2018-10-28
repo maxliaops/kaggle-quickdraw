d3487513d7fc85f29e9bbae5e31c1846ed4cd11c
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-28 11:03:00 +0100

    Revert "do not reset the data loader until completing an epoch"
    This reverts commit 72c96db

--- train.py
+++ train.py
@@ -179,12 +179,12 @@ def main():
     else:
         raise Exception("Unsupported loss type: '{}".format(loss_type))
 
-    train_set_data_loader_iter = iter(train_set_data_loader)
-
     for epoch in range(epochs_to_train):
         epoch_start_time = time.time()
 
         model.train()
+
+        train_set_data_loader_iter = iter(train_set_data_loader)
 
         train_loss_sum = 0.0
         train_accuracy_sum = 0.0
@@ -200,8 +200,7 @@ def main():
                 try:
                     batch = next(train_set_data_loader_iter)
                 except StopIteration:
-                    train_set_data_loader_iter = iter(train_set_data_loader)
-                    batch = next(train_set_data_loader_iter)
+                    break
 
                 images, categories = \
                     batch[0].to(device, non_blocking=True), \
