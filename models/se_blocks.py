f652c6e953b3b27ec81212363cf9f6f95f4f09d6
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-28 18:57:55 +0100

    added other squeeze-excitation block

--- models/se_blocks.py
+++ models/se_blocks.py
@@ -1,6 +1,16 @@
 from torch import nn
 
 
+class SpatialChannelSEBlock(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super().__init__()
+        self.cse = ChannelSEBlock(channel, reduction)
+        self.sse = SpatialSEBlock(channel)
+
+    def forward(self, x):
+        return self.cse(x) + self.sse(x)
+
+
 class ChannelSEBlock(nn.Module):
     def __init__(self, channel, reduction=16):
         super().__init__()
@@ -16,4 +26,15 @@ class ChannelSEBlock(nn.Module):
         b, c, _, _ = x.size()
         y = self.avg_pool(x).view(b, c)
         y = self.fc(y).view(b, c, 1, 1)
+        return x * y
+
+
+class SpatialSEBlock(nn.Module):
+    def __init__(self, channel):
+        super().__init__()
+        self.conv = nn.Conv2d(in_channels=channel, out_channels=1, kernel_size=1)
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.sigmoid(self.conv(x))
         return x * y
