35d7d62a24e1e3c0884b4abc797efb9bc8db7084
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-28 16:32:48 +0100

    use a pretrained resnet model

--- models/resnet.py
+++ models/resnet.py
@@ -7,7 +7,7 @@ class ResNet34(nn.Module):
     def __init__(self):
         super().__init__()
         self.expand_channels = ExpandChannels2d(3)
-        self.resnet = resnet34(pretrained=False)
+        self.resnet = resnet34(pretrained=True)
 
     def forward(self, x):
         return self.resnet(self.expand_channels(x))
