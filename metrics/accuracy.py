0891ff3b17f8bae5c9bd7075ae804985a0a6577d
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-30 18:54:38 +0100

    restore the accuracy calculation

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -4,4 +4,6 @@ from .average_precision import mapk
 
 
 def accuracy(prediction_logits, categories, topk=3):
-    return 0.
+    predictions = F.softmax(prediction_logits, dim=1)
+    _, predicted_categories = torch.topk(predictions, topk, dim=1, sorted=True)
+    return mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
