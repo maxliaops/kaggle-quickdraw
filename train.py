5fb76e7ea1bf9c0dfaab2ba91f1b653329413ece
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-30 23:19:24 +0100

    track mapk and accuracy

--- train.py
+++ train.py
@@ -1,6 +1,5 @@
 import argparse
 import datetime
-import gc
 import glob
 import os
 import sys
@@ -17,7 +16,7 @@ from torch.optim.lr_scheduler import CosineAnnealingLR, ReduceLROnPlateau
 from torch.utils.data import DataLoader
 
 from dataset import TrainDataset, TrainDataProvider
-from metrics import accuracy
+from metrics import accuracy, mapk
 from metrics.smooth_topk_loss.svm import SmoothSVM
 from models import ResNet34, SimpleCnn, MobileNetV2
 from models.drn_wrapper import Drn
@@ -48,12 +47,13 @@ def zero_item_tensor():
     return torch.tensor(0.0).float().to(device, non_blocking=True)
 
 
-def evaluate(model, data_loader, criterion, accuracy_topk):
+def evaluate(model, data_loader, criterion, mapk_topk):
     model.eval()
 
     loss_sum_t = zero_item_tensor()
-    accuracy_sum_t = zero_item_tensor()
+    mapk_sum_t = zero_item_tensor()
     accuracy_top1_sum_t = zero_item_tensor()
+    accuracy_top3_sum_t = zero_item_tensor()
     step_count = 0
 
     with torch.no_grad():
@@ -66,16 +66,18 @@ def evaluate(model, data_loader, criterion, accuracy_topk):
             loss = criterion(prediction_logits, categories)
 
             loss_sum_t += loss
-            accuracy_sum_t += accuracy(prediction_logits, categories, topk=accuracy_topk)
+            mapk_sum_t += mapk(prediction_logits, categories, topk=mapk_topk)
             accuracy_top1_sum_t += accuracy(prediction_logits, categories, topk=1)
+            accuracy_top3_sum_t += accuracy(prediction_logits, categories, topk=3)
 
             step_count += 1
 
     loss_avg = loss_sum_t.item() / step_count
-    accuracy_avg = accuracy_sum_t.item() / step_count
+    mapk_avg = mapk_sum_t.item() / step_count
     accuracy_top1_avg = accuracy_top1_sum_t.item() / step_count
+    accuracy_top3_avg = accuracy_top3_sum_t.item() / step_count
 
-    return loss_avg, accuracy_avg, accuracy_top1_avg
+    return loss_avg, mapk_avg, accuracy_top1_avg, accuracy_top3_avg
 
 
 def create_optimizer(type, model, lr):
@@ -99,7 +101,7 @@ def main():
     image_size = args.image_size
     batch_size = args.batch_size
     test_size = args.test_size
-    accuracy_topk = args.accuracy_topk
+    mapk_topk = args.mapk_topk
     num_shard_preload = args.num_shard_preload
     num_shard_loaders = args.num_shard_loaders
     num_workers = args.num_workers
@@ -120,7 +122,8 @@ def main():
     max_sgdr_cycles = args.max_sgdr_cycles
 
     train_data_provider = \
-        TrainDataProvider(input_dir, 50, num_shard_preload=num_shard_preload, num_workers=num_shard_loaders, test_size=test_size)
+        TrainDataProvider(input_dir, 50, num_shard_preload=num_shard_preload, num_workers=num_shard_loaders,
+                          test_size=test_size)
     train_data = train_data_provider.get_next()
 
     train_set = TrainDataset(train_data.train_set_df, image_size)
@@ -139,8 +142,8 @@ def main():
     print("train_set_samples: {}, val_set_samples: {}".format(len(train_set), len(val_set)), flush=True)
     print()
 
-    global_val_accuracy_best_avg = float("-inf")
-    sgdr_cycle_val_accuracy_best_avg = float("-inf")
+    global_val_mapk_best_avg = float("-inf")
+    sgdr_cycle_val_mapk_best_avg = float("-inf")
 
     optimizer = create_optimizer(optimizer_type, model, lr_max)
     lr_scheduler = CosineAnnealingLR(optimizer, T_max=sgdr_cycle_epochs, eta_min=lr_min)
@@ -164,12 +167,13 @@ def main():
         model_index = int(model_file_name.replace("model-", "").replace(".pth", ""))
         ensemble_model_index = max(ensemble_model_index, model_index + 1)
 
-    print('{"chart": "best_val_accuracy", "axis": "epoch"}')
-    print('{"chart": "val_accuracy", "axis": "epoch"}')
+    print('{"chart": "best_val_mapk", "axis": "epoch"}')
+    print('{"chart": "val_mapk", "axis": "epoch"}')
     print('{"chart": "val_loss", "axis": "epoch"}')
     print('{"chart": "val_accuracy@1", "axis": "epoch"}')
+    print('{"chart": "val_accuracy@3", "axis": "epoch"}')
     print('{"chart": "sgdr_cycle", "axis": "epoch"}')
-    print('{"chart": "accuracy", "axis": "epoch"}')
+    print('{"chart": "mapk", "axis": "epoch"}')
     print('{"chart": "loss", "axis": "epoch"}')
     print('{"chart": "lr_scaled", "axis": "epoch"}')
     print('{"chart": "mem_used", "axis": "epoch"}')
@@ -180,7 +184,7 @@ def main():
     if loss_type == "cce":
         criterion = nn.CrossEntropyLoss()
     elif loss_type == "topk_svm":
-        criterion = SmoothSVM(n_classes=len(train_data.categories), k=accuracy_topk, tau=1., alpha=1.)
+        criterion = SmoothSVM(n_classes=len(train_data.categories), k=mapk_topk, tau=1., alpha=1.)
     else:
         raise Exception("Unsupported loss type: '{}".format(loss_type))
 
@@ -192,7 +196,7 @@ def main():
         model.train()
 
         train_loss_sum_t = zero_item_tensor()
-        train_accuracy_sum_t = zero_item_tensor()
+        train_mapk_sum_t = zero_item_tensor()
 
         epoch_batch_iter_count = 0
 
@@ -211,7 +215,7 @@ def main():
 
             with torch.no_grad():
                 train_loss_sum_t += loss
-                # train_accuracy_sum_t += accuracy(prediction_logits, categories, topk=accuracy_topk)
+                # train_mapk_sum_t += mapk(prediction_logits, categories, topk=mapk_topk)
 
             optimizer.step()
 
@@ -228,23 +232,23 @@ def main():
         epoch_iterations = ceil(len(train_set) / batch_size)
 
         train_loss_avg = train_loss_sum_t.item() / epoch_batch_iter_count
-        train_accuracy_avg = train_accuracy_sum_t.item() / epoch_batch_iter_count
+        train_mapk_avg = train_mapk_sum_t.item() / epoch_batch_iter_count
 
-        val_loss_avg, val_accuracy_avg, val_accuracy_top1_avg = \
-            evaluate(model, val_set_data_loader, criterion, accuracy_topk)
+        val_loss_avg, val_mapk_avg, val_accuracy_top1_avg, val_accuracy_top3_avg = \
+            evaluate(model, val_set_data_loader, criterion, mapk_topk)
 
-        lr_scheduler_plateau.step(val_accuracy_avg)
+        lr_scheduler_plateau.step(val_mapk_avg)
 
-        model_improved_within_sgdr_cycle = val_accuracy_avg > sgdr_cycle_val_accuracy_best_avg
+        model_improved_within_sgdr_cycle = val_mapk_avg > sgdr_cycle_val_mapk_best_avg
         if model_improved_within_sgdr_cycle:
             torch.save(model.state_dict(), "{}/model-{}.pth".format(output_dir, ensemble_model_index))
-            sgdr_cycle_val_accuracy_best_avg = val_accuracy_avg
+            sgdr_cycle_val_mapk_best_avg = val_mapk_avg
 
-        model_improved = val_accuracy_avg > global_val_accuracy_best_avg
+        model_improved = val_mapk_avg > global_val_mapk_best_avg
         ckpt_saved = False
         if model_improved:
             torch.save(model.state_dict(), "{}/model.pth".format(output_dir))
-            global_val_accuracy_best_avg = val_accuracy_avg
+            global_val_mapk_best_avg = val_mapk_avg
             epoch_of_last_improval = epoch
             ckpt_saved = True
 
@@ -255,7 +259,7 @@ def main():
             sgdr_next_cycle_end_epoch = epoch + 1 + current_sgdr_cycle_epochs + sgdr_cycle_end_prolongation
 
             ensemble_model_index += 1
-            sgdr_cycle_val_accuracy_best_avg = float("-inf")
+            sgdr_cycle_val_mapk_best_avg = float("-inf")
             sgdr_cycle_count += 1
             sgdr_reset = True
 
@@ -268,9 +272,9 @@ def main():
         optim_summary_writer.add_scalar("sgdr_cycle", sgdr_cycle_count, epoch + 1)
 
         train_summary_writer.add_scalar("loss", train_loss_avg, epoch + 1)
-        train_summary_writer.add_scalar("accuracy", train_accuracy_avg, epoch + 1)
+        train_summary_writer.add_scalar("mapk", train_mapk_avg, epoch + 1)
         val_summary_writer.add_scalar("loss", val_loss_avg, epoch + 1)
-        val_summary_writer.add_scalar("accuracy", val_accuracy_avg, epoch + 1)
+        val_summary_writer.add_scalar("mapk", val_mapk_avg, epoch + 1)
 
         epoch_end_time = time.time()
         epoch_duration_time = epoch_end_time - epoch_start_time
@@ -283,18 +287,19 @@ def main():
                 get_learning_rate(optimizer),
                 train_loss_avg,
                 val_loss_avg,
-                train_accuracy_avg,
-                val_accuracy_avg,
+                train_mapk_avg,
+                val_mapk_avg,
                 int(ckpt_saved),
                 int(sgdr_reset)))
 
-        print('{"chart": "best_val_accuracy", "x": %d, "y": %.4f}' % (epoch + 1, global_val_accuracy_best_avg))
+        print('{"chart": "best_val_mapk", "x": %d, "y": %.4f}' % (epoch + 1, global_val_mapk_best_avg))
         print('{"chart": "val_loss", "x": %d, "y": %.4f}' % (epoch + 1, val_loss_avg))
-        print('{"chart": "val_accuracy", "x": %d, "y": %.4f}' % (epoch + 1, val_accuracy_avg))
+        print('{"chart": "val_mapk", "x": %d, "y": %.4f}' % (epoch + 1, val_mapk_avg))
         print('{"chart": "val_accuracy@1", "x": %d, "y": %.4f}' % (epoch + 1, val_accuracy_top1_avg))
+        print('{"chart": "val_accuracy@3", "x": %d, "y": %.4f}' % (epoch + 1, val_accuracy_top3_avg))
         print('{"chart": "sgdr_cycle", "x": %d, "y": %d}' % (epoch + 1, sgdr_cycle_count))
         print('{"chart": "loss", "x": %d, "y": %.4f}' % (epoch + 1, train_loss_avg))
-        print('{"chart": "accuracy", "x": %d, "y": %.4f}' % (epoch + 1, train_accuracy_avg))
+        print('{"chart": "mapk", "x": %d, "y": %.4f}' % (epoch + 1, train_mapk_avg))
         print('{"chart": "lr_scaled", "x": %d, "y": %.4f}' % (epoch + 1, 1000 * get_learning_rate(optimizer)))
         print('{"chart": "mem_used", "x": %d, "y": %.2f}' % (epoch + 1, psutil.virtual_memory().used / 2 ** 30))
         print('{"chart": "epoch_time", "x": %d, "y": %d}' % (epoch + 1, epoch_duration_time))
@@ -340,7 +345,8 @@ def main2():
     print(flush=True)
 
     train_data_provider = \
-        TrainDataProvider(input_dir, 50, num_shard_preload=num_shard_preload, num_workers=num_shard_loaders, test_size=test_size)
+        TrainDataProvider(input_dir, 50, num_shard_preload=num_shard_preload, num_workers=num_shard_loaders,
+                          test_size=test_size)
     train_data = train_data_provider.get_next()
 
     print("process memory used: {:.2f} GB".format(p.memory_info().rss / 2 ** 30), flush=True)
@@ -370,6 +376,7 @@ def main2():
         train_data = train_data_provider.get_next()
         train_set.df = train_data.train_set_df
 
+
 if __name__ == "__main__":
     argparser = argparse.ArgumentParser()
     argparser.add_argument("--input_dir", default="/storage/kaggle/quickdraw")
@@ -378,7 +385,7 @@ if __name__ == "__main__":
     argparser.add_argument("--epochs", default=500, type=int)
     argparser.add_argument("--batch_size", default=1024, type=int)
     argparser.add_argument("--test_size", default=0.1, type=float)
-    argparser.add_argument("--accuracy_topk", default=3, type=int)
+    argparser.add_argument("--mapk_topk", default=3, type=int)
     argparser.add_argument("--num_shard_preload", default=2, type=int)
     argparser.add_argument("--num_shard_loaders", default=1, type=int)
     argparser.add_argument("--num_workers", default=8, type=int)
