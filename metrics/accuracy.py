ee7e97bad7b922960552e4ccc94021b8455e258d
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-30 20:46:16 +0100

    improve the performance of the accuracy calculation

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -1,19 +1,13 @@
 import torch
 import torch.nn.functional as F
-from .average_precision import mapk
-
-
-# TODO: avoid redefinition
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def accuracy(prediction_logits, categories, topk=3):
     predictions = F.softmax(prediction_logits, dim=1)
     _, predicted_categories = torch.topk(predictions, topk, dim=1, sorted=True)
 
-    apk = torch.tensor(0.0).float().to(device, non_blocking=True)
-    for k in range(topk):
-        matches = torch.eq(categories, predicted_categories[:, k])
-        apk += matches.float().sum() / (k + 1)
+    apk_v = torch.eq(categories, predicted_categories[:, 0]).float()
+    for k in range(1, topk):
+        apk_v += torch.eq(categories, predicted_categories[:, k]).float() / (k + 1)
 
-    return apk / predictions.size(0)
+    return apk_v.mean()
