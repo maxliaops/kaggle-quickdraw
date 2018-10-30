5d0b39e24f2dcbf2a1c586af763c2911d603a1b4
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-30 19:05:12 +0100

    calculate the mapk on the gpu

--- metrics/accuracy.py
+++ metrics/accuracy.py
@@ -3,7 +3,16 @@ import torch.nn.functional as F
 from .average_precision import mapk
 
 
+# TODO: avoid redefinition
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+
 def accuracy(prediction_logits, categories, topk=3):
     predictions = F.softmax(prediction_logits, dim=1)
     _, predicted_categories = torch.topk(predictions, topk, dim=1, sorted=True)
-    return mapk(categories.unsqueeze(1).cpu().data.numpy(), predicted_categories.cpu().data.numpy(), k=topk)
+
+    apk = torch.tensor(0.0).float().to(device, non_blocking=True)
+    for k in range(topk):
+        apk += torch.eq(categories, predicted_categories[:, k]).sum() / float(k + 1)
+
+    return apk
