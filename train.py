8bb050cfc6948a8668dcc778f76bacc30c4ca930
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-10-31 21:30:45 +0100

    preload only one shard per default

--- train.py
+++ train.py
@@ -410,7 +410,7 @@ if __name__ == "__main__":
     argparser.add_argument("--batch_size", default=1024, type=int)
     argparser.add_argument("--test_size", default=0.1, type=float)
     argparser.add_argument("--mapk_topk", default=3, type=int)
-    argparser.add_argument("--num_shard_preload", default=2, type=int)
+    argparser.add_argument("--num_shard_preload", default=1, type=int)
     argparser.add_argument("--num_shard_loaders", default=1, type=int)
     argparser.add_argument("--num_workers", default=8, type=int)
     argparser.add_argument("--pin_memory", default=True, type=str2bool)
