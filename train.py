02dc548e409e8740d2464bd75afcb9168f3abd80
Ovidio Mallo <ovidio.mallo@netcetera.com>
2018-10-31 12:47:05 +0100

    log the image size set during progressive resizing

--- train.py
+++ train.py
@@ -204,6 +204,9 @@ def main():
         if use_progressive_image_sizes:
             next_image_size = \
                 progressive_image_sizes[min(epoch // progressive_image_epoch_step, len(progressive_image_sizes) - 1)]
+
+            print("changing image size to {}".format(next_image_size), flush=True)
+
             train_set.image_size = next_image_size
             val_set.image_size = next_image_size
 
