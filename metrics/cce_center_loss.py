993c1ec5df68c1bc48e4277b07a027f938e68078
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-11-15 00:49:18 +0100

    added a loss combining cce+center loss

---
+++ metrics/cce_center_loss.py
@@ -0,0 +1,14 @@
+import torch.nn as nn
+
+from metrics import CenterLoss
+
+
+class CceCenterLoss(nn.Module):
+    def __init__(self, num_classes, alpha):
+        super().__init__()
+        self.alpha = alpha
+        self.cce = nn.CrossEntropyLoss()
+        self.center = CenterLoss(num_classes=num_classes, feat_dim=num_classes)
+
+    def forward(self, logits, labels):
+        return self.cce(logits, labels) + self.alpha * self.center(logits, labels)
