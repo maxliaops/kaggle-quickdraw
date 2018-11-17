1ab7bde3f97e70d788e6c13ea0d37641840894b7
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-11-17 13:13:22 +0100

    persist the result of the confusion matrix calculation under /storage

--- confusion.sh
+++ confusion.sh
@@ -3,6 +3,14 @@
 set -e
 set -o pipefail
 
+if [ -z $1 ]
+then
+  echo "missing run name"
+  exit 1
+fi
+
+trap archive_artifacts EXIT
+
 function install_dependencies() {
   apt-get update >/dev/null
   apt-get -y install python3-dev libsm-dev libxrender1 libxext6 zip git >/dev/null
@@ -14,6 +22,14 @@ function install_dependencies() {
 
   pip -q install -r requirements.txt
 }
+
+function archive_artifacts() {
+  rm -rf /storage/models/quickdraw/${RUN_NAME}
+  mkdir -p /storage/models/quickdraw/${RUN_NAME}
+  cp -r /artifacts/* /storage/models/quickdraw/${RUN_NAME}
+}
+
+RUN_NAME=$1
 
 install_dependencies
 
