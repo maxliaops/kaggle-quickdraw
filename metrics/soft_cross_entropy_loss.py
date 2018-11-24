be81947b73332a8521c5e39a2bb91521151af980
Ovidio Mallo <ovidio.mallo@gmail.com>
2018-11-24 13:55:09 +0100

    support one-hot encoded categories

--- metrics/soft_cross_entropy_loss.py
+++ metrics/soft_cross_entropy_loss.py
@@ -7,6 +7,6 @@ class SoftCrossEntropyLoss(nn.Module):
     def __init__(self):
         super().__init__()
 
-    def forward(self, logits, target, target_one_hot):
+    def forward(self, logits, target):
         nll = -F.log_softmax(logits, dim=1)
-        return torch.sum(nll * target_one_hot, dim=1).mean()
+        return torch.sum(nll * target, dim=1).mean()
